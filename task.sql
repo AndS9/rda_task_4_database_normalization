d2911d12d6b332147e962391b766618b3b1eeb85
Andrii Shukalo <shukaload@gmail.com>
2026-03-29 01:40:53 +0200

    Fixes 1.0.1

--- task.sql
+++ task.sql
@@ -17,17 +17,25 @@ CREATE TABLE Warehouses (
     FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
-CREATE TABLE ProductInventory (
+CREATE TABLE Products (
     ID INT,
     ProductName VARCHAR(50),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE ProductInventory (
+    ID INT,
+    ProductID INT,
     WarehouseAmount INT,
     WarehouseID INT,
     FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE CASCADE,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL,
     PRIMARY KEY (ID)
 );
 
 -- Populate test data
-
+INSERT INTO Products(ID, ProductName)
+    VALUES (1, 'AwesomeProduct');
 INSERT INTO Countries (ID,Name)
 	VALUES (1, 'Country1');
 INSERT INTO Countries (ID,Name)
@@ -36,7 +44,7 @@ INSERT INTO Warehouses(ID, Name, Address, CountryID)
     VALUES (1,'Warehouse-1', 'City-1, Street-1', 1);
 INSERT INTO Warehouses(ID, Name, Address, CountryID)
     VALUES (2, 'Warehouse-2', 'City-2, Street-2', 2);
-INSERT INTO ProductInventory(ID,ProductName,WarehouseAmount,WarehouseID)
-	VALUES (1, 'AwesomeProduct', 2, 1);
-INSERT INTO ProductInventory(ID,ProductName,WarehouseAmount,WarehouseID)
-	VALUES (2, 'AwesomeProduct', 5,2);
+INSERT INTO ProductInventory(ID,ProductID,WarehouseAmount,WarehouseID)
+	VALUES (1, 1, 2, 1);
+INSERT INTO ProductInventory(ID,ProductID,WarehouseAmount,WarehouseID)
+	VALUES (2, 1, 5,2);
